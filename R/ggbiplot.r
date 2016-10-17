01bf477b42bb1e0462941a46480a37f5566326c2
Noah Hoffman <noah.hoffman@gmail.com>
2016-10-17 16:34:20 -0700

    add 'loadings' option to specify subset of variables displayed

--- R/ggbiplot.r
+++ R/ggbiplot.r
@@ -1,22 +1,22 @@
-# 
+#
 #  ggbiplot.r
-#  
+#
 #  Copyright 2011 Vincent Q. Vu.
-# 
+#
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License
 #  as published by the Free Software Foundation; either version 2
 #  of the License, or (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-# 
+#
 
 #' Biplot for Principal Components using ggplot2
 #'
@@ -37,6 +37,7 @@
 #' @param varname.size    size of the text for variable names
 #' @param varname.adjust  adjustment factor the placement of the variable names, >= 1 means farther from the arrow
 #' @param varname.abbrev  whether or not to abbreviate the variable names
+#' @param loadings        character vector specifying subset of variables to display as arrows
 #'
 #' @return                a ggplot2 plot
 #' @export
@@ -45,14 +46,14 @@
 #'   wine.pca <- prcomp(wine, scale. = TRUE)
 #'   print(ggbiplot(wine.pca, obs.scale = 1, var.scale = 1, groups = wine.class, ellipse = TRUE, circle = TRUE))
 #'
-ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE, 
-                      obs.scale = 1 - scale, var.scale = scale, 
-                      groups = NULL, ellipse = FALSE, ellipse.prob = 0.68, 
-                      labels = NULL, labels.size = 3, alpha = 1, 
-                      var.axes = TRUE, 
-                      circle = FALSE, circle.prob = 0.69, 
-                      varname.size = 3, varname.adjust = 1.5, 
-                      varname.abbrev = FALSE, ...)
+ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
+                      obs.scale = 1 - scale, var.scale = scale,
+                      groups = NULL, ellipse = FALSE, ellipse.prob = 0.68,
+                      labels = NULL, labels.size = 3, alpha = 1,
+                      var.axes = TRUE,
+                      circle = FALSE, circle.prob = 0.69,
+                      varname.size = 3, varname.adjust = 1.5,
+                      varname.abbrev = FALSE, loadings = NULL, ...)
 {
   library(ggplot2)
   library(plyr)
@@ -102,7 +103,7 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
     df.u <- df.u * nobs.factor
   }
 
-  # Scale the radius of the correlation circle so that it corresponds to 
+  # Scale the radius of the correlation circle so that it corresponds to
   # a data ellipse for the standardized PC scores
   r <- sqrt(qchisq(circle.prob, df = 2)) * prod(colMeans(df.u^2))^(1/4)
 
@@ -118,8 +119,8 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
   }
 
   # Append the proportion of explained variance to the axis labels
-  u.axis.labs <- paste(u.axis.labs, 
-                       sprintf('(%0.1f%% explained var.)', 
+  u.axis.labs <- paste(u.axis.labs,
+                       sprintf('(%0.1f%% explained var.)',
                                100 * pcobj$sdev[choices]^2/sum(pcobj$sdev^2)))
 
   # Score Labels
@@ -144,40 +145,44 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
   df.v$hjust = with(df.v, (1 - varname.adjust * sign(xvar)) / 2)
 
   # Base plot
-  g <- ggplot(data = df.u, aes(x = xvar, y = yvar)) + 
+  g <- ggplot(data = df.u, aes(x = xvar, y = yvar)) +
           xlab(u.axis.labs[1]) + ylab(u.axis.labs[2]) + coord_equal()
 
   if(var.axes) {
     # Draw circle
-    if(circle) 
+    if(circle)
     {
       theta <- c(seq(-pi, pi, length = 50), seq(pi, -pi, length = 50))
       circle <- data.frame(xvar = r * cos(theta), yvar = r * sin(theta))
-      g <- g + geom_path(data = circle, color = muted('white'), 
+      g <- g + geom_path(data = circle, color = muted('white'),
                          size = 1/2, alpha = 1/3)
     }
 
     # Draw directions
+    if(!is.null(loadings)){
+      df.v <- df.v[loadings,]
+    }
+
     g <- g +
       geom_segment(data = df.v,
                    aes(x = 0, y = 0, xend = xvar, yend = yvar),
-                   arrow = arrow(length = unit(1/2, 'picas')), 
+                   arrow = arrow(length = unit(1/2, 'picas')),
                    color = muted('red'))
   }
 
   # Draw either labels or points
   if(!is.null(df.u$labels)) {
     if(!is.null(df.u$groups)) {
-      g <- g + geom_text(aes(label = labels, color = groups), 
+      g <- g + geom_text(aes(label = labels, color = groups),
                          size = labels.size)
     } else {
-      g <- g + geom_text(aes(label = labels), size = labels.size)      
+      g <- g + geom_text(aes(label = labels), size = labels.size)
     }
   } else {
     if(!is.null(df.u$groups)) {
       g <- g + geom_point(aes(color = groups), alpha = alpha)
     } else {
-      g <- g + geom_point(alpha = alpha)      
+      g <- g + geom_point(alpha = alpha)
     }
   }
 
@@ -193,7 +198,7 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
       sigma <- var(cbind(x$xvar, x$yvar))
       mu <- c(mean(x$xvar), mean(x$yvar))
       ed <- sqrt(qchisq(ellipse.prob, df = 2))
-      data.frame(sweep(circle %*% chol(sigma) * ed, 2, mu, FUN = '+'), 
+      data.frame(sweep(circle %*% chol(sigma) * ed, 2, mu, FUN = '+'),
                  groups = x$groups[1])
     })
     names(ell)[1:2] <- c('xvar', 'yvar')
@@ -202,15 +207,15 @@ ggbiplot <- function(pcobj, choices = 1:2, scale = 1, pc.biplot = TRUE,
 
   # Label the variable axes
   if(var.axes) {
-    g <- g + 
-    geom_text(data = df.v, 
-              aes(label = varname, x = xvar, y = yvar, 
-                  angle = angle, hjust = hjust), 
+    g <- g +
+    geom_text(data = df.v,
+              aes(label = varname, x = xvar, y = yvar,
+                  angle = angle, hjust = hjust),
               color = 'darkred', size = varname.size)
   }
   # Change the name of the legend for groups
   # if(!is.null(groups)) {
-  #   g <- g + scale_color_brewer(name = deparse(substitute(groups)), 
+  #   g <- g + scale_color_brewer(name = deparse(substitute(groups)),
   #                               palette = 'Dark2')
   # }
 
